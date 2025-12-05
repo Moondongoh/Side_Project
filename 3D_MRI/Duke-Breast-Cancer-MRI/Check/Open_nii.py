01948235aa3e4418c2b539956a31c637aca79066
Moondongoh <mdo0421@naver.com>
2025-12-05 11:33:00 +0900

    3D data 읽어보기

--- 3D_MRI/Duke-Breast-Cancer-MRI/Check/Open_nii.py
+++ 3D_MRI/Duke-Breast-Cancer-MRI/Check/Open_nii.py
@@ -1,6 +1,4 @@
 import nibabel as nib
 
-nii = nib.load(
-    r"D:\gachon\Duke-Breast-Cancer-MRI-NIFTI\Breast_MRI_001\3.000000-ax dyn pre-93877\3_ax_dyn_pre.nii.gz"
-).get_fdata()
+nii = nib.load(r"경로지정").get_fdata()
 print(nii.shape)
